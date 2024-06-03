99672c6b4d7f6c1edb2b8ac7850a0e344a4c0c51
Chrystine Martins <chrystine.martins@outlook.com>
2024-06-03 20:20:59 -0300

    update js

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+/* 
+ OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
+
+  - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
+
+ - retirar a classe selecionado do personagem que está selecionado
+
+
+ OBJETIVO 2 - quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
+
+    - alterar a imagem do jogador 1
+    - alterar o nome do jogador 1
+*/
